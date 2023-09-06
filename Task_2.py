4d6e1cf63d225941c1f95959f81247ece76d5176
jasondsouza22 <jasondsouza2211@gmail.com>
2023-09-06 17:24:26 +0530

    Updated Task 2

--- Task_2.py
+++ Task_2.py
@@ -1,4 +1,4 @@
-# Loop through a list of five intergers and print the integer only if it is greater than 50
+# Loop through a list of five integers and print the integer only if it is greater than 50
 
 # Define the numbers
 integers = [30,40,50,60,70,80,90,100]
