7628914a0f8a24167f9a07c1d526d7f8192b0703
jasondsouza22 <jasondsouza2211@gmail.com>
2023-09-06 17:18:25 +0530

    Printing integers greater than 50

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+# Loop through a list of five intergers and print the integer only if it is greater than 50
+
+# Define the numbers
+integers = [30,40,50,60,70,80,90,100]
+
+# Loop through the list
+for num in integers:
+    if num > 50:
+        print(num)
