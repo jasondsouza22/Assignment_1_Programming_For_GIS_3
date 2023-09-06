6c99a9f797c760032326010364776bab2ba4494a
jasondsouza22 <jasondsouza2211@gmail.com>
2023-09-06 17:07:25 +0530

    Arithmetic Operation

---
+++ Task_1.py
@@ -0,0 +1,19 @@
+# Declare two numeric variable and perform arithmetic opertaions
+n1 = 16
+n2 = 8
+
+#addition
+add = n1 + n2;
+print ("The addition of {} and {} is {}".format(n1,n2,add))
+
+#subtraction
+sub = n1 - n2;
+print ("The subtraction of {} and {} is {}".format(n1,n2,sub))
+
+#multiplication
+multi = n1 * n2;
+print ("The multiplication of {} and {} is {}".format(n1,n2,multi))
+
+#division
+div = n1 / n2;
+print ("The divison of {} and {} is {}".format(n1,n2,div))
