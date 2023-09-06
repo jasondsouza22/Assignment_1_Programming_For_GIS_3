27f95c0b3e348bb72bcbe57a6f987940639cfb3f
jasondsouza22 <jasondsouza2211@gmail.com>
2023-09-06 17:22:39 +0530

    Upadated Task 1

--- Task_1.py
+++ Task_1.py
@@ -16,4 +16,4 @@ print ("The multiplication of {} and {} is {}".format(n1,n2,multi))
 
 #division
 div = n1 / n2;
-print ("The divison of {} and {} is {}".format(n1,n2,div))
+print ("The division of {} and {} is {}".format(n1,n2,div))
