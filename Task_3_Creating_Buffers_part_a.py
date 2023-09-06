b548879d50279a6fda4219c2344a81a0fceebed0
jasondsouza22 <jasondsouza2211@gmail.com>
2023-09-06 17:45:19 +0530

    Creating Buffers part A

---
+++ Task_3_Creating_Buffers_part_a.py
@@ -0,0 +1,8 @@
+import arcpy
+
+arcpy.env.workspace = r"D:\Second Year\Sem 3\Programming_for_GIS_III\New folder\Assignment_1_Programming_For_GIS_3\ProProject_Practical_One\Practical_One.gdb"
+
+# Performing buffer operations
+arcpy.analysis.Buffer("Wilson_Schools","Wilson_Schools_Buffer","500 meters")
+
+print("Process Completed")
