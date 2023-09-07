0d7176dcf28f244f94df6b5380d92f0bef755a55
jasondsouza22 <jasondsouza2211@gmail.com>
2023-09-07 13:05:11 +0530

    Updated Task_4

--- Task_4_Polygon_to_Point.py
+++ Task_4_Polygon_to_Point.py
@@ -1,11 +1,14 @@
 import arcpy
 
-# path of wilson zoning
-wilson_zoning_path = r"D:\Second Year\Sem 3\Programming_for_GIS_III\New folder\Assignment_1_Programming_For_GIS_3\ProProject_Practical_One\Practical_One.gdb\Wilson_Zoning"
+arcpy.env.workspace = r"D:\Second Year\Sem 3\Programming_for_GIS_III\New folder\Assignment_1_Programming_For_GIS_3\ProProject_Practical_One\Practical_One.gdb"
 
-# Converting wilson zone polygon to point feature
-wilson_point = wilson_zoning_path + "_FeatureToPoint"
+#Input Feature
+input_feature_class = "Wilson_Zoning"
 
-arcpy.management.FeatureToPoint(wilson_zoning_path, wilson_point)
+#Output_Feature
+output_feature_class = "Wilson_Zoning_PointFeature"
+
+
+arcpy.management.FeatureToPoint(input_feature_class, output_feature_class, "CENTROID")
 
 print("Process Completed")
